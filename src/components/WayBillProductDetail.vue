0348c159292c86c880f75009d554885c7ebbcc4b
FelicityChen <felicity.chen@oocl.com>
2019-07-26 07:43:39 +0800

    feat: can mark the package took

--- src/components/WayBillProductDetail.vue
+++ src/components/WayBillProductDetail.vue
@@ -8,7 +8,9 @@
         <el-table-column prop="status" label="状态"></el-table-column>
         <el-table-column prop="orderTime" label="预约时间"></el-table-column>
         <el-table-column prop="address">
-          <el-button round>确认收货</el-button>
+          <template slot-scope="scope">
+            <el-button round @click="comfirmTook(scope.row)">确认收货</el-button>
+          </template>
         </el-table-column>
       </el-table>
     </template>
@@ -20,6 +22,23 @@ export default {
     this.axios.get("http://localhost:8086/packages").then(response => {
       this.$store.commit("setPackagesData", response.data);
     });
+  },
+  methods: {
+    comfirmTook: function(row) {
+      row.status = "已取件";
+      this.axios
+        .post("http://localhost:8086/packages", row)
+        .then(response => {
+          this.axios.get("http://localhost:8086/packages").then(response => {
+            this.$store.commit("setPackagesData", response.data);
+            this.$store.commit("setFormData", {});
+            this.$store.commit("setDialogVisible", false);
+          });
+        })
+        .catch(error => {
+          this.$message.error("提交失败！");
+        });
+    }
   }
 };
 </script>
