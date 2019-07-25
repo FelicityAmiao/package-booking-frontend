5f2e44cc1cf1023ea58624c5f67e2a8c255a45b6
FelicityChen <felicity.chen@oocl.com>
2019-07-26 07:29:33 +0800

    feat: can select packages by status

--- src/components/Menus.vue
+++ src/components/Menus.vue
@@ -1,20 +1,46 @@
 <template>
-    <div>
-        <el-button round>ALL</el-button>
-        <el-button round>已预约</el-button>
-        <el-button round>已取件</el-button>
-        <el-button round>未取件</el-button>
-        <el-button circle @click="openAddPanel">+添加</el-button>
-    </div>
+  <div>
+    <el-button round @click="getAllPackages">ALL</el-button>
+    <el-button round @click="getBookedPackages">已预约</el-button>
+    <el-button round @click="getTookPackages">已取件</el-button>
+    <el-button round @click="getNotTakePackages">未取件</el-button>
+    <el-button circle @click="openAddPanel">+添加</el-button>
+  </div>
 </template>
 <script>
 export default {
-    methods: {
-        openAddPanel: function() {
-            this.$store.commit("setFormData", {});
-            this.$store.commit("setDialogVisible", true);
-        }
+  methods: {
+    openAddPanel: function() {
+      this.$store.commit("setFormData", {});
+      this.$store.commit("setDialogVisible", true);
+    },
+    getAllPackages: function() {
+      this.axios.get("http://localhost:8086/packages").then(response => {
+        this.$store.commit("setPackagesData", response.data);
+      });
+    },
+    getBookedPackages: function() {
+      this.axios
+        .get("http://localhost:8086/packages?status=已预约")
+        .then(response => {
+          this.$store.commit("setPackagesData", response.data);
+        });
+    },
+    getTookPackages: function() {
+      this.axios
+        .get("http://localhost:8086/packages?status=已取件")
+        .then(response => {
+          this.$store.commit("setPackagesData", response.data);
+        });
+    },
+    getNotTakePackages: function() {
+      this.axios
+        .get("http://localhost:8086/packages?status=未取件")
+        .then(response => {
+          this.$store.commit("setPackagesData", response.data);
+        });
     }
-}
+  }
+};
 </script>
 
